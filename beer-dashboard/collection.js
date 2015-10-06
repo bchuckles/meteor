b78190fe12ce52a3918fd9f8eab2bf75191aacca
bschuckles <bryantschuck@gmail.com>
2015-10-05 20:12:11 -0400

    files

---
+++ beer-dashboard/collection.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bryant.schuck on 10/5/15.
+ */
+Beers = new Meteor.Collection("Beers");
+
+Meteor.methods({
+    "insertBeer": function(numBeers, date) {
+        numBeers = parseInt(numBeers);
+
+        //check(numBeers, Number);
+        //check(date, Date);
+
+        return Beers.insert({beers: numBeers, date: date});
+    },
+
+    "removeBeer": function(id) {
+        //check(id, String);
+        return Beers.remove(id);
+    }
+})
